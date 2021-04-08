14a11ef796bd7b5d139c902703d62771d3370152
Bonnie3206 <molk3206@gmail.com>
2021-04-08 11:29:02 +0800

    Initial Commit

---
+++ healthConsultant_2/healthConsultant_2App.swift
@@ -0,0 +1,17 @@
+//
+//  healthConsultant_2App.swift
+//  healthConsultant_2
+//
+//  Created by CK on 2021/4/8.
+//
+
+import SwiftUI
+
+@main
+struct healthConsultant_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
